4786a107efad2f38f73821c2245aa31607017490
Choi Vlad <dev.choivlad@gmail.com>
2025-04-26 08:50:24 -0700

    Added winning combinations

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import {useState, useEffect} from 'react';
 import Player from "./components/Player";
 import GameBoard from "./components/GameBoard";
 import Log from "./components/Log";
+import { WINNING_COMBINATIONS } from "./winning-compinations.js";
 
 // helper function
 function deriveActivePlayer(gameTurns) {
